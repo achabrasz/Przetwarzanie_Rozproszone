634ccc5ba024913c162c971c865f55bf0d147434
achabrasz <adam.chabraszewski@gmail.com>
2024-03-06 09:58:10 +0100

    Lab_Mac_Proj

---
+++ Rozpro_Lab_1/Rozpro_Lab_1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Rozpro_Lab_1
+//
+//  Created by Adam Chabraszewski on 06/03/2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
